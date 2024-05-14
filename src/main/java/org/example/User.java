eff1eb863b5d61ff3a6198de71b5d62c5142a0fd
Lezvie <steklovrazor@yandex.ru>
2024-05-14 18:34:39 +0500

    RestTaskTemplate

---
+++ src/main/java/org/example/User.java
@@ -0,0 +1,50 @@
+package org.example;
+
+public class User {
+    private Long id;
+    private String name;
+    private  String lastName;
+    private Integer age;
+
+    public User() {
+    }
+
+    public User(Long id, String name, String surname, Integer age) {
+        this.id = id;
+        this.name = name;
+        this.lastName = surname;
+        this.age = age;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
